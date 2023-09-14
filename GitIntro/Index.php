e400c9f296cf49e440075b574b2ac7da7a97d07f
Jozsua <jkmekis@gmail.com>
2023-09-14 18:20:47 -0400

    Hello World - Git Intro

--- GitIntro/Index.php
+++ GitIntro/Index.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -6,7 +10,12 @@
     </head>
     <body>
         <?php
-        echo "Hello World!";
+        echo "<h2>Hello World!<h2>";
+        echo "</br>Jozsua Mekis, pursuing a degree in software engineering.";
+        echo "</br>I've taken multiple coding classes from OCC, such as Java, Javascript, and C++.";
+        echo "</br>I love playing video games, watching TV shows, hanging out with friends, and snuggling with my cat.";
+        echo "</br>Currently I have no plans for the Fall aside from maybe getting a job.";
+        echo "</br>I enjoy building computers, and have just recently finished a computer build for a friend.";
         ?>
     </body>
 </html>
